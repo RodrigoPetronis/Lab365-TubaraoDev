100b59fb88326d6a541e18525cda1425e30fea96
Rodrigo Petronis <107412117+RodrigoPetronis@users.noreply.github.com>
2023-12-16 21:38:19 -0300

    Add files via upload

---
+++ M01S08/Ex02/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import TaskList from "./TaskList";
+
+function App() {
+  return (
+    <div>
+     <TaskList />
+    </div>
+  );
+}
+
+export default App;
