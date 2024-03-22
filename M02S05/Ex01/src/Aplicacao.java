7354c821dfe13ed316e362813d23d7fd14044ab8
Rodrigo Petronis <rodrigopetronis@hotmail.com>
2024-03-21 21:18:32 -0300

    M02S05 EX01

---
+++ M02S05/Ex01/src/Aplicacao.java
@@ -0,0 +1,12 @@
+public class Aplicacao {
+    public void executar(){
+
+        System.out.println("---------- MENU ---------");
+        System.out.println();
+        System.out.println("1- Adicionar");
+        System.out.println("2- Listar ");
+        System.out.println("0- SAIR ");
+        System.out.println();
+        System.out.println("Digite uma opção para continuar");
+    }
+}
---
+++ M02S05/Ex01/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Aplicacao aplicacao = new Aplicacao();
+        aplicacao.executar();
+    }
+}
