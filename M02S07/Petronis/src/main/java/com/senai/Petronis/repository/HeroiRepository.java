c8f3de8158f07027fa7a0f3ae0125f158d29dc8c
Rodrigo Petronis <rodrigopetronis@hotmail.com>
2024-04-14 12:29:37 -0300

    M02S07 ex05

---
+++ M02S07/Petronis/src/main/java/com/senai/Petronis/repository/HeroiRepository.java
@@ -0,0 +1,14 @@
+package com.senai.Petronis.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.senai.Petronis.model.Heroi;
+
+@Repository
+public class HeroiRepository {
+
+    List<Heroi> herois = new ArrayList<>();
+}
