a33984cc705b58f4ea74ae031009e0940776fdff
Rodrigo Petronis <rodrigopetronis@hotmail.com>
2024-04-21 17:06:49 -0300

    Conecta com SGBD Postgres

--- M02S08/mini-projeto/Petronis/src/main/java/com/senai/Petronis/model/Heroi.java
+++ M02S08/mini-projeto/Petronis/src/main/java/com/senai/Petronis/model/Heroi.java
@@ -1,5 +1,9 @@
 package com.senai.Petronis.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +13,12 @@ import lombok.ToString;
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
+@Entity(name = "herois")
 public class Heroi {
+
+    @Id 
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Integer id;
     private String nome;
     private String superpoder;
     private Integer idade;
